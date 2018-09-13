1efabae553971b599c49a5620ae57c8d903ed65a
Diana <diana.huisen@student.hu.nl>
2018-09-13 10:58:07 +0200

    input/output en pe4_2

---
+++ les4/Input.py
@@ -0,0 +1,4 @@
+uurloon = input('Wat verdien je per uur: ')
+uuraantal = input('Hoeveel uur heb je gewerkt: ')
+salaris=float(uurloon)*float(uuraantal)
+print(uuraantal+' uur werken levert '+str(salaris)+' Euro op')
