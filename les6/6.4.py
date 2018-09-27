7dd66a2ab0ded3b6a9c3efdb07895bad6a90624f
Diana <diana.huisen@student.hu.nl>
2018-09-27 08:55:19 +0200

    les6.4 fixed

--- les6/6.4.py
+++ les6/6.4.py
@@ -1,13 +1,17 @@
 def new_password(oldpassword,newpassword):
-    cijfer1=0
     for cijfers in newpassword:
         if cijfers in '0123456789':
-            cijfer1=1
-    if newpassword != oldpassword and len(newpassword)>= 6 and cijfer1 == 1:
-        return('True')
+            cijfergevonden = True
+        else:
+            cijfergevonden = False
+    if newpassword != oldpassword and len(newpassword) >= 6 and cijfergevonden:
+        return ('Je wachtwoord is gewijzigd')
     else:
-        return('False')
+        return ('Sorry jouw wachtwoord voldoet niet aan de eisen')
+
 
 oldpassword = input('Wat is je oude password: ')
 newpassword = input('Wat is je nieuwe password: ')
 print(new_password(oldpassword, newpassword))
+
+
