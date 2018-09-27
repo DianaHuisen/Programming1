711bfa36c75be74c1d95ea912393975fedd7b9dd
Diana <diana.huisen@student.hu.nl>
2018-09-27 08:52:06 +0200

    les6.2 fixed

--- les6/6.2.py
+++ les6/6.2.py
@@ -2,7 +2,7 @@ def berekensom(lijst):
     som = 0
     for getal in lijst:
         som = som + getal
-    return(som)
+    return sum(lijst)
 
 getallenlijst=[1, 2, 3]
 print(berekensom(getallenlijst))
